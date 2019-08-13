d39e24d0177144b4fb6351be9b777a2f2a3cb131
alberts2 <alberts2@live.de>
2019-08-13 08:29:59 +0200

    Added the code from svn10967
    Co-Authored-By: billw2012 <billw2012@users.noreply.github.com>

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using FolderSelect;
+using System;
+
+namespace GetDirectory
+{
+    class Program
+    {
+        [STAThread]
+        static int Main(string[] args)
+        {
+           var openFileDialog = new FolderSelectDialog
+            {
+                Title = args.Length > 0 ? args[0] : null,
+                InitialDirectory = args.Length > 1? args[1] : null
+            };
+            if (openFileDialog.ShowDialog())
+            {
+                //Get the path of specified file
+                Console.Write(openFileDialog.FileName);
+                return 0;
+            }
+
+            return 1;
+        }
+    }
+}
